b73a9d471612d4fc746e926bbcce058189ab10a9
Antoniya <anemtserova@gmail.com>
2021-07-01 00:03:14 +0000

    first commit

--- src/models.py
+++ src/models.py
@@ -7,9 +7,10 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    favorite = db.relationship("Favorite", backref="user")
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.email
 
     def serialize(self):
         return {
@@ -17,3 +18,32 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Character(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    character_id = db.Column(db.Integer, unique=True, nullable=False)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    birth_place = db.Column(db.String(120), unique=True, nullable=False)
+
+class Planet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    planet_id = db.Column(db.Integer, unique=True, nullable=False)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    location = db.Column(db.String(120), unique=True, nullable=False)
+
+class Starship(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    starship_id = db.Column(db.Integer, unique=True, nullable=False)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    build_year = db.Column(db.Integer, unique=True, nullable=False)
+
+class Favorite(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    favorite_name = db.Column(db.String(120), unique=True, nullable=False)
+    planet_id = db.Column(db.Integer, db.ForeignKey('planet.id'))
+    starship_id = db.Column(db.Integer, db.ForeignKey('starship.id'))
+    character_id = db.Column(db.Integer, db.ForeignKey('character.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+
+    
